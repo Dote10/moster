d4882371006c6f90d6268753192801dffb53cd20
surge100 <zoqol@daum.net>
2024-02-21 16:47:23 +0900

    [➕]5가지 종속성 추가
    - swagger
    - Actuator
    -HAL Explorer
    - Spring Security

--- src/main/java/com/lunariver/monster/controller/UserController.java
+++ src/main/java/com/lunariver/monster/controller/UserController.java
@@ -3,6 +3,11 @@ package com.lunariver.monster.controller;
 import com.lunariver.monster.controller.bean.User;
 import com.lunariver.monster.dao.UserDaoService;
 import com.lunariver.monster.exception.UserNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -14,6 +19,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@Tag(name = "user-controller", description = "일반 사용자 서비스를  위한 컨트롤러 입니다.")
 public class UserController {
     private UserDaoService service;
 
@@ -26,8 +32,17 @@ public class UserController {
         return service.findAll();
     }
 
+    @Operation(summary = "사용자 정보 조회 API", description = "사용자 ID를 이용해서 사용자 상세 정보를 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "USER NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
     @GetMapping("/users/{id}")
-    public EntityModel<User> retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(
+            @Parameter(description = "사용자 ID", required = true, example = "1")
+            @PathVariable int id){
         User user = service.findOne(id);
 
         if(user == null){
